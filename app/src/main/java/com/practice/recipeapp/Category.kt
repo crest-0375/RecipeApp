5fa35778415766e22f0f6bb7eb43cd95aca2deb8
LAPTOP-VCF3S87N\JemishAKhunt <jemish.k@crestinfosystems.com>
2024-02-09 13:18:21 +0530

    Initial commit

---
+++ app/src/main/java/com/practice/recipeapp/Category.kt
@@ -0,0 +1,9 @@
+package com.practice.recipeapp
+
+data class Category(val idCategory:String,
+    val strCategory: String,
+    val strCategoryThumb: String,
+    val strCategoryDescription: String
+    )
+
+data class CategoriesResponse(val categories: List<Category>)
